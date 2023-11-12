f38c777799e02dee2e5f1421f2a9744dab92a464
AnnaDM87 <annadm.ondequadre@gmail.com>
2023-11-12 18:41:26 +0000

    Add files via upload

--- starter_file/endpoint.py
+++ starter_file/endpoint.py
@@ -3,9 +3,9 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = 'http://5be99864-5c61-4bef-8e54-dff484451595.westeurope.azurecontainer.io/score'
+scoring_uri = 'http://df676f91-6bcf-4a77-aa03-567a40949083.westus2.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = 'QDO94x11wdojWdJ3VXaG0tas2Iti9HDV'
+key = 'lIaXZhVbMKl6FkfSkJjMzt44GWlAyXPT'
 
 # Two sets of data to score, so we get two results back
 data =  {
@@ -13,11 +13,24 @@ data =  {
     "data": [
       {
         "Column2": "example_value",
-        "JoiningYear": 2017,
-        "PaymentTier": 2,
-        "Age": 33,
+        "JoiningYear": 2018,
+        "PaymentTier": 3,
+        "Age": 25,
         "ExperienceInCurrentDomain": 3,
         "Education_Masters": 1,
+        "Education_PHD": 0,
+        "Gender_Male": 1,
+        "City_New Delhi": 0,
+        "City_Pune": 0,
+        "EverBenched_Yes": 1
+      },
+      {
+        "Column2": "example_value",
+        "JoiningYear": 2015,
+        "PaymentTier": 3,
+        "Age": 22,
+        "ExperienceInCurrentDomain": 0,
+        "Education_Masters": 0,
         "Education_PHD": 0,
         "Gender_Male": 1,
         "City_New Delhi": 1,
